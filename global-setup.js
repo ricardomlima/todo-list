419ff5371d47a3e136f5f6ed4f7fbb77ad093180
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-16 20:43:29 -0300

    migrating after server start

--- global-setup.js
+++ global-setup.js
@@ -2,7 +2,7 @@ const { setup: setupDevServer } = require("jest-dev-server");
 
 module.exports = async function globalSetup() {
   await setupDevServer({
-    command: `serverless offline start`,
+    command: `serverless offline start && serverless dynamodb migrate`,
     launchTimeout: 50000,
     port: 3000,
   });
