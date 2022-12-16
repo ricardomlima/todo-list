4073dc5356b2ff917dcc3682fb7698a00ce7ba8c
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-16 20:59:08 -0300

    removing extra migration

--- global-setup.js
+++ global-setup.js
@@ -2,7 +2,7 @@ const { setup: setupDevServer } = require("jest-dev-server");
 
 module.exports = async function globalSetup() {
   await setupDevServer({
-    command: `serverless dynamodb migrate && serverless offline start`,
+    command: `serverless offline start`,
     launchTimeout: 50000,
     port: 3000,
   });
