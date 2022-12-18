01f457d24757a5ba8fc7ab43d3aa00962260b682
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-18 11:54:25 -0300

    setting local ddb port

--- global-setup.js
+++ global-setup.js
@@ -2,7 +2,7 @@ const { setup: setupDevServer } = require("jest-dev-server");
 
 module.exports = async function globalSetup() {
   await setupDevServer({
-    command: `AWS_ACCESS_KEY_ID=fake AWS_SECRET_ACCESS_KEY=fake AWS_REGION=local serverless offline start`,
+    command: `LOCAL_DDB_PORT=4566 serverless offline start`,
     launchTimeout: 50000,
     port: 3000,
   });
