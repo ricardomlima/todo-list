d3f60f68da55782ca69a91a36a4e06b12e33d9d8
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-18 11:38:01 -0300

    setting fake credentials on serverless offline start

--- global-setup.js
+++ global-setup.js
@@ -2,7 +2,7 @@ const { setup: setupDevServer } = require("jest-dev-server");
 
 module.exports = async function globalSetup() {
   await setupDevServer({
-    command: `serverless offline start`,
+    command: `AWS_ACCESS_KEY_ID=fake AWS_SECRET_ACCESS_KEY=fake AWS_REGION=local serverless offline start`,
     launchTimeout: 50000,
     port: 3000,
   });
