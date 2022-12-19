fa00988a7de853b8ab3bfefff9d53a488cb841a3
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-19 17:19:23 -0300

    increasing jest timeout correctly

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   globalSetup: "<rootDir>/global-setup.js",
   globalTeardown: "<rootDir>/global-tear-down.js",
   setupFiles: ["<rootDir>/test-config/aws-config.js"],
+  setupFilesAfterEnv: ["./jest.setup.js"],
 };
-
-jest.setTimeout(30000);
