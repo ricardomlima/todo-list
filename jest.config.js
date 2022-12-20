e319d5ce39cb70b89a5fca8c7b57544d19d3c548
Ricardo Monteiro <ricardolima89@gmail.com>
2022-12-19 21:43:33 -0300

    removing setup configuration that changed aws credentials

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   globalSetup: "<rootDir>/global-setup.js",
   globalTeardown: "<rootDir>/global-tear-down.js",
-  // setupFiles: ["<rootDir>/test-config/aws-config.js"],
   setupFilesAfterEnv: ["./jest.setup.js"],
 };
--- test-config/aws-config.js
+++
@@ -1,3 +0,0 @@
-const AWS = require("aws-sdk");
-// https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-json-file.html
-AWS.config.loadFromPath("./testconfig.json");
